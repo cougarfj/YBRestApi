74ae837f43f356d004ebab9f4cd95aa0cd48a518
root <root@10-9-84-107.(none)>
2017-07-04 18:57:46 +0800

    first commit

---
+++ RestApi/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for RestApi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append('/data/web/zh.1234tv.com/RestApi')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RestApi.settings")
+
+application = get_wsgi_application()
