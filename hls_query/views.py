c6569c1268493bb6d888090218f08b126e8a7363
Root <root@mylonly.com>
2017-07-07 17:20:33 +0800

    第一个切片可不可访问

--- hls_query/views.py
+++ hls_query/views.py
@@ -10,6 +10,7 @@ import m3u8
 from django.core.cache import cache,caches
 import logging
 import base64
+import requests
 # Create your views here.
 
 logger = logging.getLogger('django')
@@ -27,10 +28,21 @@ def query(request,hls):
             m3u8_obj = m3u8.load(hls_str)
             if m3u8_obj.is_variant:
                 for playlist in m3u8_obj.playlists:
-                    sub_m3u8_obj = m3u8.load(playlist.uri)
-            cache.set(hls_str,{"error":"0","mesage":"Ready"},60)
-            logger.info(hls_str + ': Ready')
-            return Response({"error":"0","mesage":"Ready"},status=status.HTTP_200_OK,headers={"Access-Control-Allow-Origin":"*"},content_type="application/json")
+                    m3u8_obj = m3u8.load(playlist.uri)
+                    for uri in m3u8_obj.files:
+                        file_url = m3u8_obj.base_uri+uri
+                        req = requests.get(file_url)
+                        if (req.status_code == 200):
+                            cache.set(hls_str,{"error":"0","mesage":"Ready"},60)
+                            logger.info(file_url + ': Ready')
+                            return Response({"error":"0","mesage":"Ready"},status=status.HTTP_200_OK,headers={"Access-Control-Allow-Origin":"*"},content_type="application/json")
+                        else:
+                            logger.info(file_url + ': Not Ready')
+            
+            logger.info(file_url + ':Not Ready')
+            return Response({"error":"0","mesage":"Not Ready"},status=status.HTTP_200_OK,headers={"Access-Control-Allow-Origin":"*"},content_type="application/json")
+
+            
         except Exception as err:
             logger.info(hls_str + ': Not Ready')
             return Response({"error":"1","mesage":"Not Ready"},status=status.HTTP_200_OK,headers={"Access-Control-Allow-Origin":"*"},content_type="application/json") 
